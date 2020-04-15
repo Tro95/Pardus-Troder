a3f2244a7e25c24aff7024e9f19047982614421e
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2020-04-15 17:57:31 +0100

    Removing unsafeWindow

--- pardus_troder.meta.js
+++ pardus_troder.meta.js
@@ -2,7 +2,7 @@
 // @name            Pardus Troder
 // @namespace       Tro
 // @author          Tro (Artemis)
-// @version         1.11.1
+// @version         1.11.2
 // @description     Trading script to assist in the buying and selling on planets and starbases
 // @include         *.pardus.at/starbase_trade.php
 // @include         *.pardus.at/planet_trade.php
@@ -15,16 +15,15 @@
 // @updateURL       https://github.com/Tro95/Pardus-Troder/raw/master/pardus_troder.meta.js
 // @grant           GM_setValue
 // @grant           GM_getValue
-// @grant           unsafeWindow
 // @require         https://raw.githubusercontent.com/Tro95/Pardus-Options-Library/v2.0/pardus_options_library.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.1/defaults.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.1/commodities.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.1/functions.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.1/starbase.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.1/planet.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.1/blackmarket.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.1/drop_cargo.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.1/options.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.1/ship_transfer.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.2/defaults.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.2/commodities.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.2/functions.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.2/starbase.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.2/planet.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.2/blackmarket.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.2/drop_cargo.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.2/options.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.11.2/ship_transfer.js
 //
 // ==/UserScript==
