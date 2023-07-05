1d3eee469ec69df7400f7a9ab8487232ab503302
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2023-07-05 20:01:36 +0100

    Fixing includes

--- pardus_troder.meta.js
+++ pardus_troder.meta.js
@@ -16,13 +16,13 @@
 // @grant           GM_setValue
 // @grant           GM_getValue
 // @require         https://raw.githubusercontent.com/Tro95/Pardus-Options-Library/v2.6.8/dist/pardus-options-library.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.12.11/defaults.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.12.11/commodities.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.12.11/functions.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.12.11/starbase.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.12.11/planet.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.12.11/blackmarket.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.12.11/drop_cargo.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.12.11/options.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.12.11/ship_transfer.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/defaults.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/commodities.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/functions.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/starbase.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/planet.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/blackmarket.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/drop_cargo.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/options.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/ship_transfer.js
 // ==/UserScript==
