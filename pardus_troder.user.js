2f4d0be25d27df57b8af03e54ee7d7d7a59a7906
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2025-04-24 21:21:12 +0100

    v1.14

--- pardus_troder.user.js
+++ pardus_troder.user.js
@@ -2,7 +2,7 @@
 // @name            Pardus Troder
 // @namespace       Tro
 // @author          Tro (Artemis)
-// @version         1.13
+// @version         1.14
 // @description     Trading script to assist in the buying and selling on planets and starbases
 // @match           *.pardus.at/starbase_trade.php
 // @match           *.pardus.at/planet_trade.php
@@ -16,17 +16,18 @@
 // @grant           GM_setValue
 // @grant           GM_getValue
 // @require         https://raw.githubusercontent.com/Tro95/Pardus-Options-Library/v2.6.8/dist/pardus-options-library.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/defaults.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/commodities.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/functions.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/starbase.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/planet.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/blackmarket.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/drop_cargo.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/options.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.13/ship_transfer.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.14/defaults.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.14/commodities.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.14/functions.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.14/starbase.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.14/planet.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.14/blackmarket.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.14/drop_cargo.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.14/options.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Troder/v1.14/ship_transfer.js
 // ==/UserScript==
 
+// v1.14    More key commands
 // v1.13    Adding basic key commands
 // v1.12.10 Updating Pardus Options Library
 // v1.12.9  Updating droidwashing options to display neutral div-6 cost
